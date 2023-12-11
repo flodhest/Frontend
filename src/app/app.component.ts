2f40e6cda8f5beaa29755d08d4e71c6c5340c256
flodhest <haraldtreuteger@gmail.com>
2023-12-11 12:28:04 +0100

    second

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'grid';
+  title = 'Eric Flodin';
 
+  constructor(private router: Router) {}
 
+  downloadPdf(): void {
+    // Assuming your PDF file is located in the assets folder
+    const pdfPath = '/assets/eric.pdf';
+
+    // Construct the full URL
+    const pdfUrl = window.location.origin + pdfPath;
+
+    // Open the PDF in a new tab for download
+    window.open(pdfUrl, '_blank');
+  }
 }
--- src/app/excel/excel.component.ts
+++ src/app/excel/excel.component.ts
@@ -13,76 +13,15 @@ import { ChartOptions, ChartType } from 'chart.js';
   styleUrls: ['./excel.component.scss']
 })
 export class ExcelComponent implements OnInit {
-  excelForm: FormGroup;
-  excelData: Excel[] = [];
-  templateFile: File | null = null;
 
-  chartOptions: ChartOptions = { /* your options here */ };
-  chartLegend = true;
-  chartType: ChartType = 'bar'; // Replace 'bar' with your desired chart type
+  constructor() {
 
-
-  constructor(private fb: FormBuilder, private excelService: ExcelService) {
-    this.excelForm = this.fb.group({
-      name: [''],
-      excelFile: ['']
-    });
   }
 
   ngOnInit(): void {
-    this.loadExcelData();
-    // Fetch and update chart data
-    this.fetchChartData();
+
   }
 
-  loadExcelData(): void {
-    this.excelService.getExcelData().subscribe(data => {
-      this.excelData = data;
-    });
-  }
 
-  onFileChange(event: Event): void {
-    const inputElement = event.target as HTMLInputElement;
 
-    if (inputElement.files && inputElement.files.length > 0) {
-      this.templateFile = inputElement.files[0];
-    }
-  }
-
-  uploadExcelTemplate(): void {
-    if (this.templateFile) {
-      const formData = new FormData();
-      formData.append('file', this.templateFile);
-
-      this.excelService.uploadExcelTemplate(formData).subscribe(() => {
-        // Handle success
-        console.log('Excel template uploaded successfully');
-      });
-    }
-  }
-
-  downloadExcelTemplate(): void {
-    this.excelService.downloadExcelTemplate().subscribe((data: Blob) => {
-      const blob = new Blob([data]);
-      const link = document.createElement('a');
-      link.href = window.URL.createObjectURL(blob);
-      link.download = 'your_template_file.xlsx';
-      link.click();
-    });
-  }
-
-  saveNewExcelData(): void {
-    const newExcelData: Excel = this.excelForm.value;
-    this.excelService.saveExcelData(newExcelData).subscribe(() => {
-      this.loadExcelData();
-      // Update chart data after saving new Excel data
-      this.fetchChartData();
-    });
-  }
-
-  fetchChartData(): void {
-    // Make API call or process data to update chartData and chartLabels
-    // Example: this.chartData = [...];
-    // Example: this.chartLabels = [...];
-  }
 }
