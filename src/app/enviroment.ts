d946e16522e1d34a4412d37c84170346997b9621
flodhest <haraldtreuteger@gmail.com>
2023-12-11 13:24:06 +0100

    fourth

--- src/app/enviroment.ts
+++
@@ -1,6 +0,0 @@
-// environment.ts
-export const environment = {
-  production: false,
-  apiUrl: 'https://localhost:7114/api',
-  swishApiUrl: 'https://mss.cpc.getswish.net/swish-cpcapi/api/v2/paymentrequests',
-};
